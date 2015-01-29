e904f892d18133331d5644168a6b60471464b9b0
csvan <christopher.svanefalk@gmail.com>
2015-01-29 13:13:57 +0100

    Remove irrelevant test method

--- src/test/java/com/strongloop/android/loopback/test/RestAdapterTest.java
+++ src/test/java/com/strongloop/android/loopback/test/RestAdapterTest.java
@@ -1,9 +1,6 @@
 package com.strongloop.android.loopback.test;
 
 import com.strongloop.android.loopback.RestAdapter;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class RestAdapterTest extends AsyncTestCase {
     private RestAdapter adapter;
@@ -13,22 +10,5 @@ public class RestAdapterTest extends AsyncTestCase {
         super.setUp();
         //testContext.clearSharedPreferences(RestAdapter.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
         adapter = createRestAdapter();
-    }
-
-    @Test
-    public void testAccessTokenIsStoredInSharedPreferences() {
-        final String[] accessTokenRef = new String[1];
-        adapter.setAccessToken("an-access-token");
-
-        // android-async-http client does not allow inspection of request headers
-        // the workaround is to override the setter method
-        new RestAdapter(REST_SERVER_URL) {
-            @Override
-            public void setAccessToken(String value) {
-                accessTokenRef[0] = value;
-            }
-        };
-
-        assertEquals("an-access-token", accessTokenRef[0]);
     }
 }
