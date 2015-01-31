d0ef3b8c0b886bcacea7f2197f2e712460bc4a4c
csvan <christopher.svanefalk@gmail.com>
2015-01-31 02:26:00 +0100

    Add basic login functionality for LoopbackInterface

--- src/main/java/com/strongloop/android/loopback/LoopbackInterface.java
+++ src/main/java/com/strongloop/android/loopback/LoopbackInterface.java
@@ -1,5 +1,11 @@
 package com.strongloop.android.loopback;
 
+import com.strongloop.android.loopback.callbacks.TypedCallback;
+import org.atteo.evo.inflector.English;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 /**
  * The main entry point for interacting with a Loopback server instance,
  * this class provides the funtionality needed to login as a remote user
@@ -11,6 +17,9 @@ package com.strongloop.android.loopback;
 public class LoopbackInterface {
 
     private final String url;
+    private final RestAdapter restAdapter;
+    private UserRepository<User> userRepository;
+    private User currentUser;
 
     public static LoopbackInterface getDefault(String url) {
         return new LoopbackInterface(url);
@@ -18,5 +27,105 @@ public class LoopbackInterface {
 
     private LoopbackInterface(String url) {
         this.url = url;
+
+        this.restAdapter = new RestAdapter(url);
+    }
+
+    /**
+     * Asynchronously creates an authenticated session for the Loopback server handled by this interface.
+     *
+     * @param username username/email of the user on the server
+     * @param password plainttext password for the user on the server
+     * @param callback callback upon finishing
+     */
+    public void loginAsync(String username, String password, final TypedCallback<User> callback) {
+        this.userRepository.loginUser(username, password, new UserRepository.LoginCallback<User>() {
+            @Override
+            public void onSuccess(AccessToken token, User currentUser) {
+                setCurrentUser(currentUser);
+                callback.onSuccess(currentUser);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
+
+    /**
+     * Synchronously creates an authenticated session for the Loopback server handled by this interface.
+     *
+     * @param username username/email of the user on the server
+     * @param password plainttext password for the user on the server
+     * @return true if login succeeded, false otherwise
+     */
+    public boolean loginSynchronously(String username, String password) throws LoopbackAuthenticationException {
+        if (this.userRepository == null) {
+            this.userRepository = new UserRepository<User>("User", "users", User.class);
+        }
+
+        final CountDownLatch synchLatch = new CountDownLatch(1);
+
+        this.userRepository.loginUser(username, password, new UserRepository.LoginCallback<User>() {
+            @Override
+            public void onSuccess(AccessToken token, User currentUser) {
+                setCurrentUser(currentUser);
+                synchLatch.countDown();
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                synchLatch.countDown();
+            }
+        });
+
+        while (true) {
+            try {
+                synchLatch.await(10, TimeUnit.SECONDS);
+
+                // We timed out!
+                if (synchLatch.getCount() == 0 && getCurrentUser() == null) {
+                    throw new LoopbackAuthenticationException("Login attempt timed out");
+                }
+
+                // Finished, but failed to log user in
+                else if (getCurrentUser() == null) {
+                    throw new LoopbackAuthenticationException("Login attempt failed, please check username and password");
+                }
+
+                //Succesfully logged the user in
+                else {
+                    return true;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    /**
+     * @return the User currently logged in to the Loopback instance.
+     */
+    public User getCurrentUser() {
+        return this.currentUser;
+    }
+
+    /**
+     * Gets a repository for the given model.
+     *
+     * @param modelType
+     * @param <M>
+     * @return a repository for the model of type M
+     */
+    public <M extends Model> ModelRepository<M> getRepositoryForModel(Class<M> modelType) {
+        String modelName = modelType.getSimpleName();
+        ModelRepository<M> repository = restAdapter.createRepository(modelName, English.plural(modelName), modelType);
+        repository.setAdapter(restAdapter);
+        return repository;
     }
 }
--- src/main/java/com/strongloop/android/loopback/callbacks/ObjectCallback.java
+++ src/main/java/com/strongloop/android/loopback/callbacks/ObjectCallback.java
@@ -2,8 +2,7 @@ package com.strongloop.android.loopback.callbacks;
 
 import com.strongloop.android.remoting.VirtualObject;
 
-public interface ObjectCallback<T extends VirtualObject> {
-    public void onSuccess(T object);
-    public void onError(Throwable t);
+public interface ObjectCallback<T extends VirtualObject> extends TypedCallback<T> {
+    
 }
 
